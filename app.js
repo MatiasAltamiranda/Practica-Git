cd094b335de450577ed14c41ee73341b40013124
Matias <matias_lma94@hotmail.com>
2023-09-16 12:09:53 -0300

    Mi primer commit

---
+++ app.js
@@ -0,0 +1,29 @@
+/*  
+
+= Asignando un valor 
+== Comparando si dos valores son iguales
+=== Comporando si dos valores son estrictamente iguales
+
+*/
+
+
+// OR AND
+
+// AND = &
+
+// OR = ||
+
+/* let a = 5;
+let b = 6;
+let c = 8;
+let d = 9;
+let e = 7;
+let f = 7;
+
+
+
+if(a<b || c===d && e===f){
+    console.log("La condicion se cumple")
+}else{
+    console.log("La condicicion no se cumple")
+} */
